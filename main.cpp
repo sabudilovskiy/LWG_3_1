161da8c235b9897bf2141526d893b3ed359a5973
MRV <sabudilovskiy@yandex.ru>
2021-12-19 23:27:49 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <Windows.h>
+#include <string>
+#include <stdexcept>
+#include "Graph.h"
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int A[10][10] = {{0,8,9,9,0,0,0,0,0,18},
+                     {8,0,0,9,8,0,7,0,9,10},
+                     {9,0,0,3,0,6,0,0,0,0},
+                     {9,9,3,0,2,4,0,0,0,0},
+                     {0,8,0,2,0,2,9,0,0,0},
+                     {0,0,6,4,2,0,9,0,0,0},
+                     {0,7,0,0,9,9,0,4,5,0},
+                     {0,0,0,0,0,0,4,0,1,4},
+                     {0,9,0,0,0,0,5,1,0,3},
+                     {18,10,0,0,0,0,0,4,3,0}}; //проверка дл€ остовного дерева
+    Graph graph(A, false);
+    //строим дерево
+    Tree tree = graph.find_MST();
+    std::vector<int> answer;
+    //запускаем левосторонний обход в глубину
+    tree.left_depth(answer);
+    std::cout << "Ћевосторонний обход дерева:\n";
+    for (int i = 0; i < answer.size(); i++){
+        std::cout << answer[i] << ' ';
+    }
+    std::cout << '\n';
+    answer.clear();
+    //запускаем поиск рассто€ний от 0 вершины до остальных
+    answer = graph.find_shortest_ways(0);
+    std::cout << "Ќаименьшее расто€ние от первой: \n";
+    for (int i = 0; i < answer.size(); i++) std::cout << "ƒо " << i+1 << " вершины: " << answer[i] << '\n';
+    std::cout << "—редн€€ степень по дереву: " << tree.avg_inc_left_breadth() << '\n';
+    //полностью корректна€ работа алгоритма будет только при создании фиктивных св€зей
+    Graph graph2 = Graph(A, true);
+    std::cout << "ћаксимальный поток: " << graph2.max_flow_problem(0, 9);
+}
